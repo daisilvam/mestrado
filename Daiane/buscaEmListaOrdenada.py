c6856328b0eddb196f78a83a6f5991342e444185
Daiane Silva <55991287+daisilvam@users.noreply.github.com>
2025-09-17 15:12:32 -0300

    Update buscaEmListaOrdenada.py

--- Daiane/buscaEmListaOrdenada.py
+++ Daiane/buscaEmListaOrdenada.py
@@ -1,6 +1,6 @@
-# c. Busca em lista ordenada
+
 def busca_binaria(lista, elemento):
-   #Realiza uma busca binária em uma lista ordenada.
+   
     inicio = 0
     fim = len(lista) - 1
 
@@ -8,15 +8,15 @@ def busca_binaria(lista, elemento):
         meio = (inicio + fim) // 2
         
         if lista[meio] == elemento:
-            return meio  # elemento encontrado!
+            return meio 
         elif lista[meio] < elemento:
-            inicio = meio + 1  # Busca na metade direita da lista
+            inicio = meio + 1  
         else:
-            fim = meio - 1  # Busca na metade esquerda da lista
+            fim = meio - 1 
     
-    return None  # O elemento não foi encontrado
+    return None  
 
-# Exemplo
+
 lista_ordenada = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
 elemento_procurado = 23
 
@@ -25,4 +25,5 @@ indice_encontrado = busca_binaria(lista_ordenada, elemento_procurado)
 if indice_encontrado is not None:
     print(f"O elemento {elemento_procurado} foi encontrado no índice {indice_encontrado}.")
 else:
+
     print(f"O elemento {elemento_procurado} não foi encontrado na lista.")
