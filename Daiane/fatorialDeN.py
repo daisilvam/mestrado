f194d55b4a7cea1e7c1c66e26c5e7c2e9b472387
Daiane Silva <55991287+daisilvam@users.noreply.github.com>
2025-09-17 15:13:23 -0300

    Update fatorialDeN.py

--- Daiane/fatorialDeN.py
+++ Daiane/fatorialDeN.py
@@ -1,6 +1,6 @@
-# ii. Fatorial de N
+
 def fatorial_recursivo(n):
-    # Calcula o fatorial de um número inteiro não negativo n usando recursão.
+  
     if n < 0:
         return "Erro: O fatorial não é definido para números negativos."
     elif n == 0 or n == 1:
@@ -8,12 +8,12 @@ def fatorial_recursivo(n):
     else:
         return n * fatorial_recursivo(n - 1)
 
-# uso 1
+
 numero = 5
 resultado = fatorial_recursivo(numero)
 print(f"O fatorial de {numero} é {resultado}")
 
-#Uso 2
 numero = 0
 resultado = fatorial_recursivo(numero)
+
 print(f"O fatorial de {numero} é {resultado}")
